5d58c8be91c9445490cccd1196bff169388ec30e
karthikeyan-ks <karthikeyanks3673@gmail.com>
2024-06-22 11:17:52 +0530

    fourth commit

--- strapper/app/admin.py
+++ strapper/app/admin.py
@@ -6,14 +6,13 @@ from django.db.models import TextChoices
 from django.utils.translation import gettext_lazy as _
 from unfold.views import UnfoldModelAdminViewMixin
 from django.views.generic import TemplateView
-from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 
 
 # Register your models here.
 admin.site.site_header = "Admin Panel"
 
-from app.models import Person, Course, Grade, User, JobPreference,JobOpted
+from app.models import User, JobPreference,JobOpted
 
 class UserStatus(TextChoices):
     ACTIVE = "ACTIVE", _("Active")
@@ -50,44 +49,3 @@ class JobOptedAdmin(ModelAdmin):
         result = JobPreference.objects.filter(id=obj.id).first()
         return result.title
     
-    
-        """_summary_
-@admin.register(Person)
-class PersonAdmin(ModelAdmin):
-    list_display=("last_name","first_name","show_average")
-    search_fields=("first_name",)
-    list_filter=("first_name",)
-    form = UserChangeForm
-    add_form = UserCreationForm
-    change_password_form = AdminPasswordChangeForm
-    
-        
-    def show_average(self, obj):
-        from django.db.models import Avg
-        result = Grade.objects.filter(person=obj).aggregate(Avg("grade"))
-        return result["grade__avg"]
-    
-    show_average.short_description="Average grade"
-
-    @display(
-        description=_("Status"),
-        ordering="status",
-        label={
-            UserStatus.ACTIVE: "success",  # green
-            UserStatus.PENDING: "info",  # blue
-            UserStatus.INACTIVE: "warning",  # orange
-            UserStatus.CANCELLED: "danger",  # red
-        },
-    )
-    
-    def show_status(self, obj):
-        return obj.status
-
-    @display(description=_("Status with label"), ordering="status", label=True)
-    def show_status_with_custom_label(self, obj):
-        return obj.status, obj.get_status_display()
-
-    @display(header=True)
-    def display_as_two_line_heading(self, obj):
-        return "First main heading", "Smaller additional description"
-"""
